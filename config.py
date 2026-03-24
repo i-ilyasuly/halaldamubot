05e55cc75f26ede64406c9d35a5acfcc574e6da2
Isakhan Ilyasuly <ilyasuly.isakhan@gmail.com>
2026-03-24 06:04:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # Телеграм
-BOT_TOKEN = ""
+BOT_TOKEN = "8734616545:AAGhS7IrOlTCMTH2iN286s2Gtd81UeqNx5g"
 WEBHOOK_SECRET = "momyn-secret-2024"
 
 # Гугл Жобасы
 PROJECT_ID = "momyn-t1"
 REGION = "us-central1"
 
-GEOCODING_API_KEY = ""
+GEOCODING_API_KEY = "AIzaSyCYQoJ6V2wYZm9LCAZDmaReft_dBpdo9r0"
 
 # Түнгі жаңарту паролі
 CRON_SECRET = "momyn_halal_update_777"
 
 # Gemini AI кілті
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = "AIzaSyCaMFQZbuhb2xzBdzRq3K6jEaYF2u83PkU"
 
 # Cloud Storage (Суреттер сақталатын шелек)
 BUCKET_NAME = "momyn-vision-assets"
